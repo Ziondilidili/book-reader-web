e7fb60fe8738ba02d026121292ff2422b2a9a257
simplessor <1003659191@qq.com>
2021-07-04 22:04:07 +0800

    fix(book-list): add route for book-list
    display book-list-root component when path is /list

--- projects/book-reader-root/src/app/book-list/book-list-routing.module.ts
+++ projects/book-reader-root/src/app/book-list/book-list-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListRootComponent } from './book-list-root/book-list-root.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    /**
+     * 书籍列表
+     */
+    path:"",
+    pathMatch:"full",
+    component:BookListRootComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

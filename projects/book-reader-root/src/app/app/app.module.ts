36dcf518e0e3c78d7f564d20e07479b91cc003d4
simplessor <1003659191@qq.com>
2021-07-04 21:46:07 +0800

    support material design
    add @angular/material

--- projects/book-reader-root/src/app/app/app.module.ts
+++ projects/book-reader-root/src/app/app/app.module.ts
@@ -4,7 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { environment } from "projects/book-reader-root/src/environments/environment";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { environment } from '../environments/environment';
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]

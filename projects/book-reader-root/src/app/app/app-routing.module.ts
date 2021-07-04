1168ba9e2b5a3f8440e481e531c82106793a5bfc
simplessor <1003659191@qq.com>
2021-07-04 21:55:32 +0800

    dev(module): link root&book-list

--- projects/book-reader-root/src/app/app/app-routing.module.ts
+++ projects/book-reader-root/src/app/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    /**
+     * 书籍列表模块
+     */
+    path:"list",
+    loadChildren:()=>import("projects/book-reader-root/src/app/book-list/book-list.module").then(m=>m.BookListModule)
+  },{
+    /**
+     * 如果访问根目录
+     * 则自动重定向到书籍列表模块
+     */
+    path:"",
+    pathMatch:"full",
+    redirectTo:"list"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

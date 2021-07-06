7898e9855e2f58407944900392899be031db52c0
simplessor <1003659191@qq.com>
2021-07-07 00:28:37 +0800

    feat: update the initial BookInfo
    update currnetIndex from 0 to 1

--- projects/book-reader-root/src/app/app/entity/book-info.ts
+++ projects/book-reader-root/src/app/app/entity/book-info.ts
@@ -4,7 +4,7 @@ export class BookInfo {
     totalIndex:number
     description?:string
     ctime:Date
-    constructor(name:string,currentIndex:number=0,totalIndex:number=1,description?:string,ctime:Date=new Date()){
+    constructor(name:string,currentIndex:number=1,totalIndex:number=0,description?:string,ctime:Date=new Date()){
         this.name = name
         this.currentIndex = currentIndex
         this.totalIndex = totalIndex

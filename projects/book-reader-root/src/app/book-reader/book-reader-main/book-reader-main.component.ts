bfb31da470be15062341192770e38582ccb9336d
simplessor <1003659191@qq.com>
2021-07-07 10:42:31 +0800

    feat(View): add header bar for reader interface

--- projects/book-reader-root/src/app/book-reader/book-reader-main/book-reader-main.component.ts
+++ projects/book-reader-root/src/app/book-reader/book-reader-main/book-reader-main.component.ts
@@ -11,9 +11,12 @@ import { BookService } from '../../app/services/book.service';
 })
 export class BookReaderMainComponent implements OnInit {
   chapter?:Chapter
+  bookInfo?:BookInfo
+  bookName:string = "loading"
   title:string = "loading"
   content:string = ""
   // paragraphs:string[] = []
+  operable:boolean = false
   constructor(
     private bookService:BookService,
     private activatedRoute:ActivatedRoute,
@@ -21,10 +24,13 @@ export class BookReaderMainComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.initChapter()
+    this.loadChapter()
   }
 
-  private async initChapter(){
+  /**
+   * 通过路由参数加载章节信息
+   */
+  private async loadChapter(){
     this.activatedRoute.paramMap.subscribe(async paramMap=>{
       const bookName = paramMap.get("bookName") 
       const title = paramMap.get("title")
@@ -42,10 +48,22 @@ export class BookReaderMainComponent implements OnInit {
     })
   }
 
+  /**
+   * 控制可操作性
+   */
+   toggleOperability(){
+    this.operable = !this.operable
+  }
+
+
+  /******************************************工具函数 临时使用**********************************************/
+
   private resolveChapter(chapter:Chapter,bookInfo:BookInfo){
     this.chapter = chapter
     this.title = chapter.title
     this.content = chapter.content.trim()
+    this.bookInfo = bookInfo
     // this.paragraphs = this.content.split(/[\f\n\r]+/g)
   }
+
 }

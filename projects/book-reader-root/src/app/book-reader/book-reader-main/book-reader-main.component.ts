d47179df37e98e0c9960a38ea13b2038ef00f86f
simplessor <1003659191@qq.com>
2021-07-07 10:03:52 +0800

    dev: add bookInfo into book-reader-main

--- projects/book-reader-root/src/app/book-reader/book-reader-main/book-reader-main.component.ts
+++ projects/book-reader-root/src/app/book-reader/book-reader-main/book-reader-main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { BookInfo } from '../../app/entity/book-info';
 import { Chapter } from '../../app/entity/chapter';
 import { BookService } from '../../app/services/book.service';
 
@@ -12,7 +13,7 @@ export class BookReaderMainComponent implements OnInit {
   chapter?:Chapter
   title:string = "loading"
   content:string = ""
-  paragraphs:string[] = []
+  // paragraphs:string[] = []
   constructor(
     private bookService:BookService,
     private activatedRoute:ActivatedRoute,
@@ -20,34 +21,31 @@ export class BookReaderMainComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe(paramMap=>{
+    this.initChapter()
+  }
+
+  private async initChapter(){
+    this.activatedRoute.paramMap.subscribe(async paramMap=>{
       const bookName = paramMap.get("bookName") 
       const title = paramMap.get("title")
       if(!bookName||!title){
         this.router.navigate(["/list"])
         return
       }
-      this.bookService.getChapterWithTitle(bookName,title)
-      .then(chapter=>{
-        if(!chapter){
-          this.router.navigate(["/page-not-found"])
-          return
-        }
-        // this.chapter = chapter
-        this.resolveChapter(chapter)
-      })
+      const bookInfo = await this.bookService.getBookInfo(bookName)
+      const chapter = await this.bookService.getChapterWithTitle(bookName,title)
+      if(!chapter){
+        this.router.navigate(["/page-not-found"])
+        return
+      }
+      this.resolveChapter(chapter,bookInfo)
     })
   }
 
-  resolveChapter(chapter:Chapter){
+  private resolveChapter(chapter:Chapter,bookInfo:BookInfo){
+    this.chapter = chapter
     this.title = chapter.title
     this.content = chapter.content.trim()
-    this.paragraphs = this.content.split(/[\f\n\r]+/g)
+    // this.paragraphs = this.content.split(/[\f\n\r]+/g)
   }
-
-  // getBookInfo(bookName:string){
-  //   const info = this.bookService.getBookInfo(bookName)
-  //   console.log(info)
-  // }
-
 }

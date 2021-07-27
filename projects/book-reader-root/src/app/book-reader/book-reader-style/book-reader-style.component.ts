3e0f9be0ac6caffbad074ff44fd3074656abf82c
simplessor <1003659191@qq.com>
2021-07-27 11:54:53 +0800

    feat: update fontSize , fontWeight

--- projects/book-reader-root/src/app/book-reader/book-reader-style/book-reader-style.component.ts
+++ projects/book-reader-root/src/app/book-reader/book-reader-style/book-reader-style.component.ts
@@ -1,14 +1,104 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { of, Subject, Subscription } from 'rxjs';
+import { concatAll, map } from "rxjs/operators"
+import { Config } from '../../app/entity/config';
+import { ConfigService } from '../../app/services/config.service';
 
 @Component({
   selector: 'book-reader-book-reader-style',
   templateUrl: './book-reader-style.component.html',
   styleUrls: ['./book-reader-style.component.css']
 })
-export class BookReaderStyleComponent implements OnInit {
-  constructor(
-  ) { }
-
-  ngOnInit(): void {
+export class BookReaderStyleComponent implements OnInit,OnDestroy {
+  // 配置名与环境变量名映射
+  private configNameAndEnvNameMap = {
+    fontSizePX:"content.fontSize.px",
+    fontWeight:"content.fontWeight",
+    fontFamily:"content.fontFamily",
+    textIndentEM:"content.textIndent.em",
+    fontColor:"content.fontColor",
+    bgColor:"content.bgColor",
+    lineHeightEM:"content.lineHeight.em"
   }
+  // 配置表
+  styleConfigMap = new Map<string,Config>()
+  // 配置订阅列表
+  private subscriptionList:Subscription[] = []
+
+  constructor(
+    private configService:ConfigService
+  ) { }
+  ngOnInit(): void {
+    // 订阅样式配置
+    this.subscribeStyleConfig()
+    
+  }
+  ngOnDestroy(): void {
+    // 取消所有配置订阅
+    this.subscriptionList.forEach(subscription=>subscription.unsubscribe)
+  }
+  // 订阅样式配置
+  private async subscribeStyleConfig(){
+    of(...Object.values(this.configNameAndEnvNameMap))
+    .pipe(
+      map(envName=>this.configService.getObservableConfig(envName)),
+      concatAll()
+    ).subscribe(subject=>{
+      const subscription = subject.subscribe(config=>{
+        this.styleConfigMap.set(config.name,config)
+      })
+      this.subscriptionList.push(subscription)
+    })
+  }
+  // 更改字体大小
+  updateFontSizePX(dsize:number){
+    const config = this.styleConfigMap.get("content.fontSize.px")
+    if(!config)return;
+    config.value+=dsize
+    this.configService.updateObservableConfig(config)
+  }
+  // 更改是否粗体
+  updateFontWeight(fontWeight:string){
+    const config = this.styleConfigMap.get("content.fontWeight")
+    if(!config)return;
+    config.value = fontWeight
+    this.configService.updateObservableConfig(config)
+  }
+  // 更改字体
+  updateFontFamily(fontFamily:string){
+    const config = this.styleConfigMap.get("content.fontFamily")
+    if(!config)return;
+    config.value = fontFamily
+    this.configService.updateObservableConfig(config)
+  }
+  // 更改缩进
+  updateTextIndentEM(textIndentEM:number){
+    const config = this.styleConfigMap.get("content.textIndent.em")
+    if(!config)return;
+    config.value = textIndentEM
+    this.configService.updateObservableConfig(config)
+  }
+  // 更改字体颜色
+  updateFontColor(fontColor:string){
+    const config = this.styleConfigMap.get("content.fontColor")
+    if(!config)return;
+    config.value = fontColor
+    this.configService.updateObservableConfig(config)
+  }
+  // 更改背景颜色
+  updateBgColor(bgColor:string){
+    const config = this.styleConfigMap.get("content.bgColor")
+    if(!config)return;
+    config.value = bgColor
+    this.configService.updateObservableConfig(config)
+  }
+  // 更改行高
+  updateLineHeightEM(lineHeightEM:number){
+    const config = this.styleConfigMap.get("content.lineHeight.em")
+    if(!config)return;
+    config.value = lineHeightEM
+    this.configService.updateObservableConfig(config)
+  }
+
+
 }
